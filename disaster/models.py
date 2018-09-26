3dead10f1e0d0229b9be7a558bb620421217ee85
Aravind Balakrishnan <aravindbalan222@gmail.com>
2018-09-26 14:38:15 +0530

    2

--- disaster/models.py
+++ disaster/models.py
@@ -1,3 +1,75 @@
-from django.db import models
+
 
 # Create your models here.
+
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from .managers import UserManager
+from django.conf import settings
+
+state_choices = (("Andhra Pradesh","Andhra Pradesh"),("Arunachal Pradesh ","Arunachal Pradesh "),("Assam","Assam"),("Bihar","Bihar"),("Chhattisgarh","Chhattisgarh"),("Goa","Goa"),("Gujarat","Gujarat"),("Haryana","Haryana"),("Himachal Pradesh","Himachal Pradesh"),("Jammu and Kashmir ","Jammu and Kashmir "),("Jharkhand","Jharkhand"),("Karnataka","Karnataka"),("Kerala","Kerala"),("Madhya Pradesh","Madhya Pradesh"),("Maharashtra","Maharashtra"),("Manipur","Manipur"),("Meghalaya","Meghalaya"),("Mizoram","Mizoram"),("Nagaland","Nagaland"),("Odisha","Odisha"),("Punjab","Punjab"),("Rajasthan","Rajasthan"),("Sikkim","Sikkim"),("Tamil Nadu","Tamil Nadu"),("Telangana","Telangana"),("Tripura","Tripura"),("Uttar Pradesh","Uttar Pradesh"),("Uttarakhand","Uttarakhand"),("West Bengal","West Bengal"),("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),("Chandigarh","Chandigarh"),("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),("Daman and Diu","Daman and Diu"),("Lakshadweep","Lakshadweep"),("National Capital Territory of Delhi","National Capital Territory of Delhi"),("Puducherry","Puducherry"))
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'),blank=False,null=False)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    phone = models.CharField(_('phone'),max_length=30,unique=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff status'),default=False)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+class state(models.Model):
+    name = models.CharField(choices=state_choices,max_length=100)
+class district(models.Model):
+    state = models.ForeignKey(state, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    
+
+
+
+class Disasters(models.Model):
+    name = models.CharField(max_length=30,blank=False,null=False)
+    State = models.ForeignKey(state, on_delete=models.SET_NULL, null=True)
+    District = models.ForeignKey(district, on_delete=models.SET_NULL, null=True)
+
+ 
+
+
+
+
+
+
--- innv/settings.py
+++ innv/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'disaster',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+AUTH_USER_MODEL = 'disaster.User'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
